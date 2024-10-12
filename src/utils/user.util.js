8733435d04b27ab8a289de31a41d748270cc06ff
chinaza-max <mosesogbonna68@gmail.com>
2024-10-12 22:13:28 +0100

    test

--- src/utils/user.util.js
+++ src/utils/user.util.js
@@ -23,21 +23,18 @@ class UserUtil {
       size: Joi.number().positive().less(3000000).optional(),
     }).optional(),
     type: Joi.string().valid('landLord', 'agent', 'unset').required().label('Type'),
-  
     agentBankCode: Joi.string().when('type', {
       is: 'agent',
       then:Joi.required().label('Agent Bank Code') ,
       otherwise:  Joi.forbidden(),
     }),
-  
     agentBankAccount: Joi.string().when('type', {
       is: 'agent',
       then:Joi.required().label('Agent Bank Account'),
       otherwise: Joi.forbidden(), 
     }),
-
     landlordBankCode: Joi.string().required().label('Landlord Bank Code'),
-    landlordBankAccount:Joi.required().label('Landlord Bank Account'),
+    landlordBankAccount:Joi.string().required().label('Landlord Bank Account'),
     companyName: Joi.string().required().label('Company Name'),
     agentRegistrationNO: Joi.string().required().label('Agent Registration Number'),
   });
