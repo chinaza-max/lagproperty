ef6bd671ab086af5c93586e6cf031cf5b18bf569
chinaza-max <mosesogbonna68@gmail.com>
2024-10-17 18:13:17 +0100

    test

--- src/db/index.js
+++ src/db/index.js
@@ -40,7 +40,7 @@ class DB {
     if (serverConfig.NODE_ENV === "development") {
       // await this.sequelize.sync({ alter: true });   
       // await this.sequelize.sync({ force: true }); 
-    } 
+    }   
                     
 /*     
         (async () => {
