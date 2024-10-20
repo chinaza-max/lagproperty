3d8a08d9e1cdf8b7a252801e0b980131d15af9dd
chinaza-max <mosesogbonna68@gmail.com>
2024-10-20 12:45:19 +0100

    test

--- src/utils/user.util.js
+++ src/utils/user.util.js
@@ -14,7 +14,7 @@ class UserUtil {
     tel: Joi.number().required().label('Telephone Number'),
     telCode: Joi.string().required().label('Telephone Code'),
     lasrraId: Joi.string().required().label('LASRRA ID'),
-    nin: Joi.number().required().label('NIN'),
+    nin: Joi.string().required().label('NIN'),
     about: Joi.string().required().label('Information about your self and building'),
     country: Joi.string().required().label('Country'),
     state: Joi.string().required().label('State'),
@@ -64,7 +64,7 @@ class UserUtil {
     occupation: Joi.string().required(),
     country: Joi.string().required(),
     stateOfOrigin: Joi.string().required(),
-    nin: Joi.number().integer().required(),
+    nin: Joi.string().integer().required(),
     bankCode: Joi.string().required(),
     bankAccount: Joi.string().required(),
     propertyPreference: Joi.array().items(
