7199a8ab919b5eb541e8fd39de336ed526143a68
chinaza-max <mosesogbonna68@gmail.com>
2025-01-20 01:04:15 -0800

    adding rate limiter

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -692,6 +692,7 @@
 import { Router } from"express";
 import AuthController from "../controllers/auth/auth.controller.js";
 import uploadHandler from "../middlewares/upload.middleware.js";
+import { rateLimit } from 'express-rate-limit';
 
 class AuthRoutes extends AuthController {
   constructor() {
@@ -701,6 +702,11 @@ class AuthRoutes extends AuthController {
   }
 
   routes() {
+    const loginLimiter = rateLimit({
+      windowMs: 3 * 60 * 1000, // 15 minutes
+      max: 3, // limit each IP to 5 requests per windowMs
+    });
+
 
     this.router.post("/registerUser", this.signupUser);
     this.router.post("/verifyEmailorTel", this.verifyEmailorTel);
